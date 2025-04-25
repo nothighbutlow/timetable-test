8a7ba9fa90863541860aa895a8956f2f07522418
nothighbutlow <mslowqiaoyuan@gmail.com>
2025-04-25 19:26:23 +0800

    Add files via upload

---
+++ timetable_data_3in.py
@@ -0,0 +1,16 @@
+parsed_timetable = {
+    'Mon': {
+        'TBD': ["Mon FT 3 EX Hafizhah LUNCH SS G3 3 IN ChiaYX MA G3", "Mon FT 3 EX TaniaChia MA G3 3 IN ChiaYX LUNCH EL G3 3 EX Hafizhah"],
+    },
+    'Tue': {
+        'TBD': ["Tue FT EL G3 AM2 3 IN Hanna LUNCH CHEM2 3 RE,BIOLAB2 Latifah MA G3", "Tue FT EL G3 AM2 3 IN Hanna 3 EX TaniaChia LUNCH Self Study MA G3"],
+    },
+    'Wed': {
+        'TBD': ["Wed NSueraSShyiYtaiMSki ar/a rSi a/h hPe /he WSluaai v/Ma SBHha e/l arRanAol y/z niT  /aW SnoBaolSmn  /ia/  ThSa i/nl iaSiaPaCunhathihai TL G3 PEG SS G3 3 IN ChiaYX LUNCH EL G3"],
+    },
+    'Thu': {
+        'TBD': ["Thu FT EL G3 3 IN Hanna PEG LUNCH MA G3 CCE TL G3", "Thu FT EL G3 3 IN Hanna PEG LUNCH MA G3 CCE TL G3", "Thu FT PEG 3 IN Hanna 3 IN ChiaYX LUNCH CCE EL G3 SS G3", "Thu FT EL G3 3 IN Hanna SC_CM3 3 EX Hafizhah LUNCH CCE TL G3"],
+    },
+    'Fri': {
+    },
+}
